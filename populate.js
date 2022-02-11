283e216d54b58c136f4d275bbdc1675beb5e5f96
KingKellee <kellyiyogun@gmail.com>
2022-02-11 22:13:27 +0100

    minor refactoring of populate.js

--- populate.js
+++ populate.js
@@ -9,10 +9,13 @@ const jsonProducts = require('./products.json');
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
     await Product.create(jsonProducts);
-    console.log('Product has been populated to the database');
+    process.exit(0); // this instructs node to terminate the process if successful
+    // console.log('Product has been populated to the database');
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
